d3afb72a83108921525ed2931c27c1388c323d04
Tanase Alex <alextanase60@yahoo.com>
2025-02-01 22:06:56 +0200

    test automation

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+}).then(() => console.log('MongoDB connected !'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 app.use(cors())
